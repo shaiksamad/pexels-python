7c9667c057f441c32c9f1c1d3b1ac06e58d41a90
Samad <41123191+shaiksamad@users.noreply.github.com>
2023-07-25 21:11:40 +0530

    converted to package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="python_pexels",
+    version="0.1.0",
+    description="Pexels API in python.",
+    author="Shaik Abdus Samad",
+    author_email="shaiksamad.dev@gmail.com",
+    packages=find_packages(),
+)
